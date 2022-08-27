4c7c0dd1ecb965320e53521f45f7fcbe6eaa7b1f
paulohltc <phltc@cin.ufpe.br>
2022-08-27 18:27:42 -0300

    oxe

--- code/front/src/app/app-routing.module.ts
+++ code/front/src/app/app-routing.module.ts
@@ -1,7 +1,56 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+import { ClienteHomeComponent } from './components/cliente/cliente-home/cliente-home.component';
+import { ClienteShoppingCartComponent } from './components/cliente/cliente-shopping-cart/cliente-shopping-cart.component';
+import { ClienteDebitCardComponent } from './components/cliente/cliente-debit-card/cliente-debit-card.component';
+import { ClienteProfilePageComponent } from './components/cliente/cliente-profile-page/cliente-profile-page.component';
+import { AdminProductsComponent } from './components/empresa/admin-products/admin-products.component';
+import { AdminSalesComponent } from './components/empresa/admin-sales/admin-sales.component';
+import { AdminUsersComponent } from './components/empresa/admin-users/admin-users.component';
+import { AdminProfilePageComponent } from './components/empresa/admin-profile-page/admin-profile-page.component';
+
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent
+  },
+
+  // Rotas de cliente
+  {
+    path: 'cliente-home', component: ClienteHomeComponent
+  },
+  {
+    path: 'shopping-cart', component: ClienteShoppingCartComponent
+  },
+  {
+    path: 'debit-card', component: ClienteDebitCardComponent
+  },
+  {
+    path: 'cliente-profile-page', component: ClienteProfilePageComponent
+  },
+
+  // Rotas de funcionários
+  {
+    path: 'products', component: AdminProductsComponent
+  },
+  {
+    path: 'sales', component: AdminSalesComponent
+  },
+  {
+    path: 'users', component: AdminUsersComponent
+  },
+  {
+    path: 'admin-profile-page', component: AdminProfilePageComponent
+  },
+
+
+
+  // redirect to home
+  {
+    path: '**', redirectTo: 'login'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- code/front/src/app/app.module.ts
+++ code/front/src/app/app.module.ts
@@ -3,13 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
 import { FormsModule } from '@angular/forms';
+
+
+import { LoginComponent } from './components/login/login.component';
+import { ClienteHomeComponent } from './components/cliente/cliente-home/cliente-home.component';
+import { ClienteShoppingCartComponent } from './components/cliente/cliente-shopping-cart/cliente-shopping-cart.component';
+import { ClienteDebitCardComponent } from './components/cliente/cliente-debit-card/cliente-debit-card.component';
+import { ClienteProfilePageComponent } from './components/cliente/cliente-profile-page/cliente-profile-page.component';
+import { AdminProductsComponent } from './components/empresa/admin-products/admin-products.component';
+import { AdminSalesComponent } from './components/empresa/admin-sales/admin-sales.component';
+import { AdminUsersComponent } from './components/empresa/admin-users/admin-users.component';
+import { AdminProfilePageComponent } from './components/empresa/admin-profile-page/admin-profile-page.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    LoginComponent,
+    ClienteHomeComponent,
+    ClienteShoppingCartComponent,
+    ClienteDebitCardComponent,
+    ClienteProfilePageComponent,
+    AdminProductsComponent,
+    AdminSalesComponent,
+    AdminUsersComponent,
+    AdminProfilePageComponent
   ],
   imports: [
     BrowserModule,
