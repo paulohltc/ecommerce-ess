b8765df1e4e65fa9af37a852b12dc5f9f5b7d39e
CynaraCosta <cvocac@cin.ufpe.br>
2022-08-28 17:54:47 -0300

    login and register

--- code/front/src/app/components/login/login.component.ts
+++ code/front/src/app/components/login/login.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  public showPassword: boolean = false;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
   }
 
 }
--- code/front/src/app/components/register/register.component.ts
+++ code/front/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  public showPassword: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
   }
+  public togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
 
 }
+
