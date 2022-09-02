44e47e2a2c420602349856081f719d3970c0b02c
CynaraCosta <cvocac@cin.ufpe.br>
2022-09-02 17:30:50 -0300

    added the home page

--- code/front/src/app/components/cliente/cliente-home/cliente-home.component.ts
+++ code/front/src/app/components/cliente/cliente-home/cliente-home.component.ts
@@ -1,5 +1,11 @@
 import { Component, ComponentFactoryResolver, OnInit } from '@angular/core';
 import { LoggedService } from 'src/app/services/logged/logged.service';
+import { Product } from 'src/app/models/product';
+import { ProductsService } from 'src/app/services/products/products.service';
+import { formatPrice } from 'src/app/utils/utils';
+import { Navigation, NavigationExtras, Router } from '@angular/router';
+import { MediaMatcher } from '@angular/cdk/layout';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 @Component({
@@ -11,9 +17,33 @@ export class ClienteHomeComponent implements OnInit {
 
   nome: string = "palao";
   idade: number = 21;
+  formatPrice = formatPrice;
 
+  displayedColumns: string[] = ['name', 'category', 'price', 'carrinho'];
+  dataSourceProducts = new MatTableDataSource(this.getProducts());
 
-  constructor(private loggedService: LoggedService) { }
+  constructor(private loggedService: LoggedService, private media: MediaMatcher, private productsService: ProductsService, private router: Router) { }
+
+  getProducts(): Product[] {
+    let products: Product[] = [];
+    this.productsService.getProducts().subscribe(productsList => products = productsList);
+    return Array.from(products.values());
+  }
+
+  getcodefromIndex(index: number): string {
+    console.log(this.dataSourceProducts.data[index].code);
+    return this.dataSourceProducts.data[index].code;
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSourceProducts.filter = filterValue.trim().toLowerCase();
+  }
+
+  addProductToCart(code: string): void {
+    //  servico do carrinho
+    this.router.navigateByUrl('/shopping-cart')
+  }
 
   ngOnInit(): void {
   }
