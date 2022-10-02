aba8cb9373f57a3dff396193f8a78f1edcabea32
CynaraCosta <cvocac@cin.ufpe.br>
2022-10-02 16:04:05 -0300

    feature: added the gui cliente-purchase forms

--- code/front/src/app/components/cliente/cliente-purchase/cliente-purchase.component.ts
+++ code/front/src/app/components/cliente/cliente-purchase/cliente-purchase.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cliente-purchase',
@@ -7,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientePurchaseComponent implements OnInit {
 
-  constructor() { }
+  public enderecoForm: FormGroup = this.formBuilder.group({
+    name: new FormControl('', [Validators.required]),
+    CPF: new FormControl('', [Validators.required, Validators.minLength(11)]),
+    CEP: new FormControl('', [Validators.required, Validators.minLength(8)]),
+    Endereco: new FormControl('', [Validators.required]),
+    numero: new FormControl('', [Validators.required]),
+    complemento: new FormControl('', [Validators.required])
+  });;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
